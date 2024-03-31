b8e2ad3befc9fcc4f8a0ea93f0132fa3de39a244
fnascimentosilva <fabricionsilva26@gmail.com>
2024-03-31 17:40:30 -0400

    adicionando primeiro algoritmo media aluno

---
+++ modulo_1/2_semana_aula_1/mediaAluno.js
@@ -0,0 +1,10 @@
+var nota1 = Number(prompt("Digite a primeira nota: "))
+var nota2 = Number(prompt("Digite a segunda nota: "))
+var nota3 = Number(prompt("Digite a terceira nota: "))
+var nota4 = Number(prompt("Digite a quarta nota: "))
+
+
+var media = ((nota1+nota2+nota3+nota4)/4)
+
+
+console.log(media);
