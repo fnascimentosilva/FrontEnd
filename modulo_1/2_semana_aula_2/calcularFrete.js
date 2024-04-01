a8021397a245eeb7ad0481bd63408c65b7b200e6
fnascimentosilva <fabricionsilva26@gmail.com>
2024-03-31 20:50:58 -0400

    adicionando comentario

--- modulo_1/2_semana_aula_2/calcularFrete.js
+++ modulo_1/2_semana_aula_2/calcularFrete.js
@@ -1,3 +1,11 @@
+/*Considere as seguintes informações:
+
+A empresa cobra uma taxa fixa de R$0,50 por quilo do produto.
+
+Além disso, a empresa cobra uma taxa adicional de R$0,10 por quilômetro da distância percorrida.
+
+O custo total do frete é a soma do custo do peso com o custo da distância.*/
+
 var peso = prompt("Digite o peso do produto: ")
 var distancia = prompt("Insira a distancia a ser percorrida")
 
