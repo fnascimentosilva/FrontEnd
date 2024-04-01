ee0693dcbf99285064637a18b8592d04c2adb59b
fnascimentosilva <fabricionsilva26@gmail.com>
2024-04-01 16:30:44 -0400

    #

--- modulo_1/2_semana_aula_3/api/index.js
+++
@@ -1,9 +0,0 @@
-async function consultarCep(){
-    const url = "https://viacep.com.br/ws/69088591/json/"
-    const response = await fetch(url)
-    const json = await response.json()
-
-    console.log(json.logradouro)
-}
-
-consultarCep()
