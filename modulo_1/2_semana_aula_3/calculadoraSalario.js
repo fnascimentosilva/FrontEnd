1e23a8688df5d17b5291a6857ec4b0f8b230e653
fnascimentosilva <fabricionsilva26@gmail.com>
2024-04-01 00:53:43 -0400

    adicionando calculo salario liquido

---
+++ modulo_1/2_semana_aula_3/calculadoraSalario.js
@@ -0,0 +1,31 @@
+var salario = prompt("Digite o valor do salário: ")
+
+var impostoRenda = 0
+
+if(salario <= 1903.98){
+    impostoRenda = impostoRenda
+}else if(salario >= 1903.99 && salario <= 2826.65){
+    impostoRenda = (salario * 0.075)-142.8
+}else if(salario >= 2826.66 && salario <= 3751.05){
+    impostoRenda = (salario * 0.15)-354.8
+}else if(salario >= 3751.06 && salario <= 4664.68){
+    impostoRenda = (salario * 0.225)-636.13
+}else{
+    impostoRenda = (salario * 0.275)-869.36
+}263.87
+
+var inss = 0
+
+if(salario <= 1302){
+    inss = salario * 0.075
+}else if(salario >= 1302.01 && salario <= 2571.29){
+    inss = salario * 0.09
+}else if(salario >= 2571.30 && salario <= 3856.94){
+    inss = salario * 0.12
+}else{
+    inss = salario * 0.14
+}
+
+var salarioLiquido = salario - impostoRenda - inss
+
+console.log(salarioLiquido)
